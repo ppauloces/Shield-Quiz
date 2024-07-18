4ccca757dbb6bbb2a7a5ce896be441c655a66aec
ppauloces <ppaulo.developer@gmail.com>
2024-07-18 18:40:08 -0300

    first

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import PlayerInput from './components/PlayerInput.vue';
+
+// Cria uma referência para armazenar o nome do jogador
+const playerName = ref('');
+
+// Tempo de expiração em milissegundos (exemplo: 1 hora)
+const EXPIRATION_TIME = 60 * 60 * 1000;
+
+// Função para salvar o nome do jogador no localStorage com um timestamp
+function setPlayerName(name) {
+  const now = new Date().getTime();
+  const data = {
+    name: name,
+    timestamp: now
+  };
+  localStorage.setItem('playerName', JSON.stringify(data));
+  playerName.value = name;
+}
+
+// Função para carregar o nome do jogador do localStorage
+function loadPlayerName() {
+  const data = JSON.parse(localStorage.getItem('playerName'));
+  if (data) {
+    const now = new Date().getTime();
+    // Verifica se o dado está expirado
+    if (now - data.timestamp < EXPIRATION_TIME) {
+      playerName.value = data.name;
+    } else {
+      // Remove o dado expirado
+      localStorage.removeItem('playerName');
+    }
+  }
+}
+
+// Carrega o nome do jogador ao montar o componente
+onMounted(() => {
+  loadPlayerName();
+});
+</script>
+
+<template>
+  <div id="app">
+    <div v-if="!playerName">
+      <PlayerInput @submitName="setPlayerName" />
+    </div>
+    <div v-else>
+      <h1>Bem-vindo, {{ playerName }}!</h1>
+    </div>
+  </div>
+</template>
+
+
---
+++ src/components/PlayerInput.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue';
+import ArrowRightThickIcon from './icons/ArrowRightThickIcon.vue';
+
+const playerName = ref('');
+
+const emit = defineEmits(['submitName']);
+
+function submitName() {
+  if (playerName.value.trim() !== '') {
+    emit('submitName', playerName.value);
+    playerName.value = '';
+  } else {
+    alert('Por favor, digite um nome');
+  }
+}
+</script>
+
+<template>
+  <div class="player-input-container">
+    <div class="flex items-center justify-center h-screen">
+      <input v-model="playerName" type="text" placeholder="Digite seu nome"
+        class="player-input p-2 border border-gray-300 rounded-md bg-gray-800 text-white mr-2" />
+      <button @click="submitName"
+        class="player-button">
+        <ArrowRightThickIcon></ArrowRightThickIcon>
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.player-input-container {
+  background-color: #1B1B1B;
+  @apply flex flex-col items-center justify-center h-screen text-white;
+}
+
+.player-input {
+  @apply p-2 border border-gray-300 rounded-md bg-gray-800 text-white;
+}
+
+.player-button {
+  @apply bg-indigo-500 text-white font-bold py-2 px-4 rounded hover:bg-indigo-700;
+}
+</style>
